85ca1ab15dd9887d91825c728744baa5611324b0
Markus-Chen <2253059449@qq.com>
2019-05-29 16:56:34 +0800

    commit

--- 拓扑生成/Form1.Designer.cs
+++ 拓扑生成/Form1.Designer.cs
@@ -34,9 +34,9 @@ namespace 拓扑生成
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,7 +47,7 @@ namespace 拓扑生成
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(162, 33);
             this.button1.TabIndex = 0;
-            this.button1.Text = "导入结点-弧拓扑";
+            this.button1.Text = "导入弧-结点拓扑";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -58,7 +58,7 @@ namespace 拓扑生成
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(162, 33);
             this.button2.TabIndex = 1;
-            this.button2.Text = "导入弧-结点拓扑";
+            this.button2.Text = "导入结点-弧拓扑";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -106,15 +106,14 @@ namespace 拓扑生成
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "运算结果";
             // 
-            // label3
+            // richTextBox2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(6, 166);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(153, 19);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "2.弧-多边形拓扑";
+            this.richTextBox2.Location = new System.Drawing.Point(10, 188);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(451, 179);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
             // richTextBox1
             // 
@@ -124,14 +123,15 @@ namespace 拓扑生成
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
             // 
-            // richTextBox2
+            // label3
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(10, 188);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(451, 179);
-            this.richTextBox2.TabIndex = 6;
-            this.richTextBox2.Text = "";
-            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(6, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "2.弧-多边形拓扑";
             // 
             // Form1
             // 
--- 拓扑生成/Form1.cs
+++ 拓扑生成/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -39,7 +40,28 @@ namespace 拓扑生成
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            //初始化所有点信息
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    Point[i, j] = "";
+            OpenFileDialog o = new OpenFileDialog();
+            o.ShowDialog();
+            string filename = o.FileName;
+            StreamReader reader = null;
+            reader = new StreamReader(filename);
+            string line = reader.ReadLine();
+            for (int i = 0; i < 4; line = reader.ReadLine())
+            {
+                line = new Regex("[\\s]+").Replace(line, "");
+                string[] str = line.Split(' ');
+                for (int j = 0; j < 4; j++)
+                {
+                    Point[i, j] = str[j];
+                }
+                i++;
+            }
+            if (reader != null)
+                reader.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -53,13 +75,118 @@ namespace 拓扑生成
             string filename = o.FileName;
             StreamReader reader = null;
             reader = new StreamReader(filename);
-
-
+            string line = reader.ReadLine();
+            for(int i=0;i<6;line=reader.ReadLine())
+            {
+                line = new Regex("[\\s]+").Replace(line, "");
+                string[] str = line.Split(' ');
+                Side[i] = new Boundary(str[0], str[1], str[2]);
+                i++;
+            }
+            if (reader != null)
+                reader.Close();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            string N0, Nc;
+            for(int i=0,j=1;i<6;i++)
+            {
+                if(Side[i].Pl=="")
+                {
+                    richTextBox1.Text += "P" + j + " " + Side[i].name + " ";
+                    Side[i].Pl = "P" + j;
+                    N0 = Side[i].S;
+                    Nc = Side[i].E;
+                    string A = Side[i].name;
+                    int B = 0;
+                    while(N0!=Nc)
+                    {
+                        for(int m=0;m<4;m++)
+                        {
+                            if(Point[m,0]==Nc)
+                            {
+                                for(int n=1;n<4;n++)
+                                {
+                                    if(Point[m,n]==A)
+                                    {
+                                        if (n == 3)
+                                            n = 0;
+                                        B = Convert.ToInt16(Point[m, n + 1]);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        richTextBox1.Text += Side[B - 1].name + " ";
+                        if(Nc==Side[B-1].S)
+                        {
+                            Side[B - 1].Pl = "P" + j;
+                            Nc = Side[B - 1].E;
+                            A = Side[B - 1].name;
+                        }
+                        else
+                        {
+                            Side[B - 1].Pr = "P" + j;
+                            Nc = Side[B - 1].S;
+                            A = Side[B - 1].name;
+                        }
+                        if (N0 == Nc)
+                            break;
+                    }
+                    j++;
+                    richTextBox1.Text += "\n";
+                }
+                if(Side[i].Pr=="")
+                {
+                    richTextBox1.Text += "P" + j + " " + Side[i].name + " ";
+                    Side[i].Pl = "P" + j;
+                    N0 = Side[i].E;
+                    Nc = Side[i].S;
+                    string A = Side[i].name;
+                    int B = 0;
+                    while (N0 != Nc)
+                    {
+                        for (int m = 0; m < 4; m++)
+                        {
+                            if (Point[m, 0] == Nc)
+                            {
+                                for (int n = 1; n < 4; n++)
+                                {
+                                    if (Point[m, n] == A)
+                                    {
+                                        if (n == 3)
+                                            n = 0;
+                                        B = Convert.ToInt16(Point[m, n + 1]);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        richTextBox1.Text += Side[B - 1].name + " ";
+                        if (Nc == Side[B - 1].S)
+                        {
+                            Side[B - 1].Pl = "P" + j;
+                            Nc = Side[B - 1].E;
+                            A = Side[B - 1].name;
+                        }
+                        else
+                        {
+                            Side[B - 1].Pr = "P" + j;
+                            Nc = Side[B - 1].S;
+                            A = Side[B - 1].name;
+                        }
+                        if (N0 == Nc)
+                            break;
+                    }
+                    j++;
+                    richTextBox1.Text += "\n";
+                }
+            }
+            for(int i=0;i<6;i++)
+            {
+                richTextBox2.Text += "弧段" + Side[i].name + " 左多边形为：" + Side[i].Pl + " 右多边形为：" + Side[i].Pr + "\n";
+            }
         }
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
